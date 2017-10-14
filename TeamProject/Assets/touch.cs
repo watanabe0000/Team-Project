9eae565a6671444b4dbc3a44cd4e29cf3da962c3
PS16A15\vantan <arashilikeyma.com@ezweb.ne.jp>
2017-10-14 15:30:57 +0900

    watanabe

---
+++ TeamProject/Assets/touch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class touch : MonoBehaviour {
+
+    GameObject red;
+    GameObject black;
+
+    Vector3 mouse_position;
+
+    private void Start()
+    {
+        red = GameObject.Find("red");
+        black = GameObject.Find("black");
+    }
+
+    void Update () {
+
+        
+       
+
+	}
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+    }
+}
