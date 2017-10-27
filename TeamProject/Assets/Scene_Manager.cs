bfef2a1feec77c9d26b854a74a65efc255054295
Akinori Miura <ariant615@gmail.com>
2017-10-27 11:28:45 +0900

    あああ

--- TeamProject/Assets/Scene_Manager.cs
+++ TeamProject/Assets/Scene_Manager.cs
@@ -6,16 +6,7 @@ using UnityEngine.SceneManagement;
 public class Scene_Manager : MonoBehaviour {
 
 	
-	void Start () {
-
-        GameObject chara_red = GameObject.Find("赤");
-        GameObject chara_black = GameObject.Find("黒");
-
-        chara_red.SetActive(true);
-        chara_black.SetActive(true);
-
-
-    }
+	
 
      void Update()
     {
